06ad2bc324c156e1309a9858e02ed0fbb591c68f
Bob Haslett <bob.haslett@ft.com>
2017-05-15 12:31:07 +0100

    Update

--- build/g-yaxislinear.js
+++ build/g-yaxislinear.js
@@ -6,7 +6,7 @@
 
     function yaxisLinear() {
 
-        let yScale = d3.d3.scaleLinear();
+        let yScale = d3.scaleLinear();
         let yAxisAlign = "right"
         let yLabelOffset = 0;
         let tickSize = 5;
